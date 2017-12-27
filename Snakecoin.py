0f8d847e8ac03e1e52a21b180103e61f2f38b659
TIANXIANG LIU <tianxiangliu@TIANXIANGs-MacBook-Pro.local>
2017-12-27 10:11:45 +0800

    snackcoin

---
+++ Snakecoin.py
@@ -0,0 +1,36 @@
+import hashlib as hasher
+
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(str(self.index) +
+                   str(self.timestamp) +
+                   str(self.data) +
+                   str(self.previous_hash))
+
+
+import datetime as date
+
+
+def create_genesis_block():
+    '''
+    Mannual construct a block with
+    index zero and arbitary previous hash
+    '''
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Block " + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
